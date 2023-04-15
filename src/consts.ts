6610afd2d0e1e35efd7b5ba053a41e3a750e383f
nelson patrao <nelson.patrao@gmail.com>
2023-04-15 22:49:26 +0200

    can create an image

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,5 @@
 
 export const SITE_TITLE = 'My personal website.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const BEARER_TOKEN = 'eaaee52512d5885e2f301c3d39b314eda624605e6c94e190c5a21a28dd4a5065f5a652940320ae8a19c2ad4499bcf54d86c931f83dbff7c57c26fd0c3b920bf7b5dfaa9eea973c8863b2070d33e46582ed88c6266aa57243583a95564ae3465009362aec750c1aa6df841e4e3edb9db6c337474d4f5bb44514728f2db06485bf';
+export const API_URL = 'http://localhost:1337/api/';
