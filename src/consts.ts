f5ae08fc24fe2fff86246a31bb2f6be84bd390e8
nelson patrao <nelson.patrao@gmail.com>
2023-04-21 19:05:29 +0200

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@
 
 export const SITE_TITLE = 'Bad Journey';
 export const SITE_DESCRIPTION = 'Welcome to the Bad Journey!';
-export const BEARER_TOKEN = '1d469a6347bc6acca51bef642fbe1377aea753ccac7b1d4a57036c85207c52146b8e216b4d84306e31dfb1fe8e4f3051fb16a5694757da3e7c63de14866a17d455c047aa0839608f7443145fc72714fc3421f3b8f32a574fc90bc3682972db9569bbf0101bed50a3ec2b34d6baca93af0f8b8d0e37f151e35ef2460a2f2622c5';
+export const BEARER_TOKEN = '0d84b585b5083f6222b18f2c5c3831f7828960c8c334e6d764f485729107253e64130f0d35ecae5a911803b1178714d056e34d2fe11a635f28e69b4a2cc108d2ecf7feb650faa08df63b35bce746b0999da87bfc920e531f0a0f46ee5dba37897bdfc275b34306161ba47e3b06fff1f3ef482d87d3f76b0d5ece8b31f437d3ee';
 export const API_URL = 'https://hammerhead-app-377qb.ondigitalocean.app/api/';
 export const IMAGES_URL = 'https://hammerhead-app-377qb.ondigitalocean.app';
 // export const API_URL = 'http://localhost:1337/api/';
