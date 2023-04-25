7cb7cf486c890135b04486809c0895386e61fcca
angeenes <nelson.patrao@gmail.com>
2023-04-25 23:20:11 +0200

    update tokens

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,8 @@
 
 export const SITE_TITLE = 'Bad Journey';
 export const SITE_DESCRIPTION = 'Welcome to the Bad Journey!';
-export const BEARER_TOKEN = '5bf7d14bc73c6c5f8e58e5cba359606e00e55f75b3665920b622ad78c917c933b3c152d584304dbe490adf05e3cd2eed071dae6a68356790a44f47400daf3a56f5d45922d5f3b2f34dc2e4be2c6dd3f479f13c863183ef3a4e9e960e83d5522ea4a0ff4fbf00145ab9733ea65ffdbad6e4d5e9877db18be37313b9214b71ea8d';
+export const BEARER_TOKEN = '23cd7aed3639338ee56bee6179519c6699fabbdb65ab70ebf73934f2b0186e72f14655060675a11b0e703cc8070a69055805326a291f927212cec6134f42ac3af4945731e4ca14683c54d59b46011831b940f7918fe65594ef8aa9d8e48ea0daaf62aca8a77e93491e6b99dc452cfe7a98100f86af78cd5ac2a56ec13193d7ae';
+export const BEARER_TOKEN_POST_IMAGES = '46d0d9dcc3c02b89bcbc4a74623efcabfc8e1e5dafa9ebd0da0dce767008e0dd516be93391a194c91b602142f90a3ba50481a5a65096cd1534a22896e71162c460f184ebb9513c834aff6213f480bb35eb64fe72fc3c8374ae9f27e09836aa8bf4a6a77e62d67f23526b6e67dba3f2552db101093a515bd3b97d19b379d20bf0'
 export const API_URL = 'https://hammerhead-app-377qb.ondigitalocean.app/api/';
 export const IMAGES_URL = 'https://hammerhead-app-377qb.ondigitalocean.app';
 // export const API_URL = 'http://localhost:1337/api/';
