c1163bdfb4e3f629a406af162a2840f8c8eb2b3f
angeenes <nelson.patrao@gmail.com>
2023-04-25 23:11:05 +0200

    fix regExp Automatic prompt Exif extractor

--- src/assets/js/converters/convertAutomatic111Metadata.ts
+++ src/assets/js/converters/convertAutomatic111Metadata.ts
@@ -2,40 +2,51 @@ import { ImageObject } from "../../types/ImageObject";
 
 export default function convertAutomatic111Metadata(input: string): ImageObject {
 
-  console.log('convertAutomatic111Metadata REÇOIS :       ', input);
+  console.log('DEBUG : convertAutomatic111Metadata REÇOIS :      ', input);
 
-  // Expression régulière pour extraire la largeur et la hauteur de l'image
+  // Définition des expressions régulières pour chaque information à extraire
   const sizeRegExp = /Size: (\d+)x(\d+)/;
-
-  // Expression régulière pour extraire les informations de la ligne "Steps"
-  const stepsRegExp = /Steps: (\d+), Sampler: (\w+) ([a-z]), CFG scale: (\d+), Seed: (\d+), (.+)/i;
-
-
-  const sizeMatch = input.match(sizeRegExp);
-  const stepsMatch = input.match(stepsRegExp);
+  const stepsRegExp = /Steps: (\d+)/i;
+  const modelRegExp = /Model: (.+)/i;
+  const seddRegExp = /Seed: (\d+)/i;
+  const gfcScaleRegExp = /scale:\s*(\d+)/i;
+  const samplerRegExp = /Sampler: (\w+) ([a-z])/i
 
   // Extraction des informations du texte d'entrée en utilisant les expressions régulières
-  const [, width, height] = sizeMatch || [];
-  const [, steps, generator, cfg_scale, seed, model] = stepsMatch || []; // handle null by providing a default empty array
+  const sizeMatch = input.match(sizeRegExp);
+  const stepsMatch = input.match(stepsRegExp);
+  const modelMatch = input.match(modelRegExp);
+  const seedMatch = input.match(seddRegExp);
+  const gfcScaleMatch = input.match(gfcScaleRegExp);
+  const samplerMatch = input.match(samplerRegExp);
 
+  // Attribution des valeurs correspondantes à chaque information extraite
+  const [, widthValue, heightValue] = sizeMatch || [];
+  const [, stepsValue] = stepsMatch || [];
+  const [, modelValue] = modelMatch || [];
+  const [, seedValue] = seedMatch || [];
+  const [, cfg_scaleValue] = gfcScaleMatch || [];
+  const [, samplerValue] = samplerMatch || [];
+
+  // Extraction de la prompt négative, si elle existe
   const negativePromptMatch = input.match(/Negative prompt: (.+)/);
   const negativePrompt = negativePromptMatch ? negativePromptMatch[1] : "";
 
-  // Création de l'objet "ImageObject"
+  // Création de l'objet "ImageObject" en utilisant les valeurs extraites et la prompt d'entrée
   const output: ImageObject = {
     prompt: input.split("\n")[0],
     negative_prompt: negativePrompt,
-    model: model || "",
-    seed: seed || "",
-    width: width,
-    height: height,
-    cfg_scale: cfg_scale || "",
-    steps: steps || "",
+    model: modelValue || "",
+    seed: seedValue || "",
+    width: widthValue,
+    height: heightValue,
+    cfg_scale: cfg_scaleValue || "",
+    steps: stepsValue || "",
     generator: 'Automatic111',
+    sampler: samplerValue || ""
   };
 
-  console.log('ouput', output);
-
+  console.log('DEBUG output', output);
 
   return output;
 }
--- src/assets/types/ImageObject.ts
+++ src/assets/types/ImageObject.ts
@@ -1,11 +1,12 @@
 export interface ImageObject {
-    prompt: string;
-    negative_prompt?: string;
-    model?: string;
-    seed?: string;
-    width: string;
-    height: string;
-    cfg_scale?: string;
-    steps?: string;
-    generator: string;
-  }
+  prompt: string;
+  negative_prompt?: string;
+  model?: string;
+  seed?: string;
+  width: string;
+  height: string;
+  cfg_scale?: string;
+  steps?: string;
+  generator: string;
+  sampler?: string;
+}
