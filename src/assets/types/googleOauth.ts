972f101ab8606b90214186cfdffa56ece415c839
nelson patrao <nelson.patrao@gmail.com>
2023-04-26 22:24:34 +0200

    up

---
+++ src/assets/types/googleOauth.ts
@@ -0,0 +1,10 @@
+export interface GoogleOauth {
+    id_token: string
+    access_token: string
+    "raw%5Baccess_token%5D": string
+    "raw%5Bexpires_in%5D": string
+    "raw%5Bscope%5D": string
+    "raw%5Btoken_type%5D": string
+    "raw%5Bid_token%5D": string
+  }
+  
