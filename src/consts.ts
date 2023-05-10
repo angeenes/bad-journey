fdfecc12e7736e8bdd4affb8026265d7a6650322
nelson patrao <nelson.patrao@gmail.com>
2023-05-10 21:48:56 +0200

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@
 
 export const SITE_TITLE = 'Bad Journey';
 export const SITE_DESCRIPTION = 'Welcome to the Bad Journey!';
-export const BEARER_TOKEN = 'c1209f1d25c54359247082ebe9e89d45c064c1d5ecf4fce5fa8666ac7234d9cf2ec4ae3a1f91f13b0f0881a0a7396ac98b61ee231bfea24a8bbb4d943e7236d4bc350a79fd76d8afe456ddd5db2f79314842b3a82a630cc4d7ae1686affde4783bff84d5d1a062e4e574745b561bd957f698d67c2a95d5e206d54135ab230e61';
+export const BEARER_TOKEN = `${import.meta.env.PUBLIC_BEARER_TOKEN}`
 export const BEARER_TOKEN_POST_IMAGES = `${import.meta.env.PUBLIC_BEARER_TOKEN_POST_IMAGES}`
 export const API_URL = `${import.meta.env.PUBLIC_API_URL}`;
 export const IMAGES_URL = `${import.meta.env.PUBLIC_IMAGES_URL}`;
