b6dba24f461fbf03eef44b729640f0b0f8860526
nelson patrao <nelson.patrao@gmail.com>
2023-05-10 21:44:33 +0200

    add env development

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,8 @@
 
 export const SITE_TITLE = 'Bad Journey';
 export const SITE_DESCRIPTION = 'Welcome to the Bad Journey!';
-export const BEARER_TOKEN = '9fd40e4ed3e317912f9649c64b4408844f4aa83eec99b3ff683cb6342b00c5f53824097dc4ac58d9ccedc061a01f2fba1bfff74f794bd628c22277c7390184f5a5bf0ff4e071c9e41e638f4c26f1fff259c974db76bc6989efab7d594c3f1265140e3c1379b92c0259392abbe3c49196805458a41a1118a107bd67d45ebaf9e0';
-export const BEARER_TOKEN_POST_IMAGES = '74f0b4f4eb00107a42d91e4cf98a738b0f96e743955085bd4901b9c39cb652fa23a1e1d505c331676a7fe7ba38fb43cdb4b4d649fb0918ddb3ef6de8218bac9d4e5877edc54b9333b3e00c64f9bebc9dd0607679abacac9795c2d51f78f279e70c1ad6c5488caf258e414aefafb562269db028f6d336839dbc1e3a4ee50e890e'
-export const API_URL = 'https://hammerhead-app-377qb.ondigitalocean.app/api';
-export const IMAGES_URL = 'https://hammerhead-app-377qb.ondigitalocean.app';
-export const WEB_APP_URL = 'https://bd-ai-boom.netlify.app';
-// export const API_URL = 'http://localhost:1337/api/';
+export const BEARER_TOKEN = 'c1209f1d25c54359247082ebe9e89d45c064c1d5ecf4fce5fa8666ac7234d9cf2ec4ae3a1f91f13b0f0881a0a7396ac98b61ee231bfea24a8bbb4d943e7236d4bc350a79fd76d8afe456ddd5db2f79314842b3a82a630cc4d7ae1686affde4783bff84d5d1a062e4e574745b561bd957f698d67c2a95d5e206d54135ab230e61';
+export const BEARER_TOKEN_POST_IMAGES = `${import.meta.env.PUBLIC_BEARER_TOKEN_POST_IMAGES}`
+export const API_URL = `${import.meta.env.PUBLIC_API_URL}`;
+export const IMAGES_URL = `${import.meta.env.PUBLIC_IMAGES_URL}`;
+export const WEB_APP_URL = `${import.meta.env.PUBLIC_WEB_APP_URL}`;
